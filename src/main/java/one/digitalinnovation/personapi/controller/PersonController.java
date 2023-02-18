00b1432b8e129f91f38e99ddac3fa1ecb616b273
Roberta de Siqueira <designer.roberta@gmail.com>
2023-02-18 09:45:11 -0300

    Improved person creation functionality

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,12 +1,16 @@
 package one.digitalinnovation.personapi.controller;
 
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -21,13 +25,13 @@ public class PersonController {
 
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
-  public MessageResponseDTO findAllPerson() {
+  public List<Person> findAllPerson() {
     return personService.findAllPerson();
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public MessageResponseDTO createPerson(@RequestBody Person person) {
-    return personService.createPerson(person);
+  public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+    return personService.createPerson(personDTO);
   }
 }
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,10 +1,14 @@
 package one.digitalinnovation.personapi.service;
 
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -12,13 +16,16 @@ public class PersonService {
 
   private PersonRepository personRepository;
 
+  private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
   @Autowired
   public PersonService(PersonRepository personRepository) {
     this.personRepository = personRepository;
   }
 
-  public MessageResponseDTO createPerson(Person person) {
-    Person savedPerson = personRepository.save(person);
+  public MessageResponseDTO createPerson(PersonDTO personDTO) {
+    Person personToSave = personMapper.toModel(personDTO);
+    Person savedPerson = personRepository.save(personToSave);
     return MessageResponseDTO
             .builder()
             .message("Created person with ID " + savedPerson.getId())
